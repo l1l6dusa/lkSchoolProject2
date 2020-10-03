a2bb605027c5998b480aea062ff1a3e9764ebefa
l1l6dusa <63962252+l1l6dusa@users.noreply.github.com>
2020-10-03 03:25:45 +0300

    project finalization

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private float _spawnTimeRange;
+
+    private SpawnPoint[] _spawnPoints;
+    private float _timePassed;
+
+    private void Start()
+    {
+        _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+    }
+
+    private void Update()
+    {
+        _timePassed += Time.deltaTime;
+        if (_timePassed > _spawnTimeRange)
+        {
+            InitiateSpawn(Random.Range(0, _spawnPoints.Length));
+            _timePassed = 0;
+        }
+    }
+
+    private void InitiateSpawn(int spawnIndex)
+    {
+        _spawnPoints[spawnIndex].Spawn();
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [Range(0,3)][SerializeField] private float _spawnAreaRadius;
+    [SerializeField] private GameObject _spawnObject;
+    
+    public void Spawn()
+    {
+        
+       
+        
+            var spawnPosition = new Vector2(
+                Random.Range(transform.position.x-_spawnAreaRadius, transform.position.x+_spawnAreaRadius),
+                Random.Range(transform.position.y-_spawnAreaRadius,transform.position.y+_spawnAreaRadius)
+                );
+            Instantiate(_spawnObject, spawnPosition, Quaternion.identity).transform.SetParent(this.transform);
+
+    }
+}
+    
+
